db3a0575d59bdada2f8a3af14458cadd95b4b1e1
Roselle Hsu <lifuh@andrew.cmu.edu>
2024-07-04 22:06:47 -0400

    Updated some files with recent changes on 7/4

--- sql/dump_raw_schemas.sql
+++ sql/dump_raw_schemas.sql
@@ -1,10 +1,26 @@
-.echo on --ensures that the SQL commands being executed are printed to the output
+.echo on -- Ensures that the SQL commands being executed are printed to the output
 
--- Describe the schema of the following tables:
-DESCRIBE yellow_tripdata;
-DESCRIBE green_tripdata;
-DESCRIBE fhvhv_tripdata;
-DESCRIBE fhv_tripdata;
-DESCRIBE citibike_tripdata;
-DESCRIBE central_park_weather;
-DESCRIBE fhv_bases;
+-- Show all the tables in the current database
+SHOW TABLES;
+
+-- Describe all schemas
+SELECT 'bike_data';
+DESCRIBE "bike_data";
+
+SELECT 'central_park_weather';
+DESCRIBE "central_park_weather";
+
+SELECT 'fhv_bases';
+DESCRIBE "fhv_bases";
+
+SELECT 'fhv_tripdata';
+DESCRIBE "fhv_tripdata";
+
+SELECT 'fhvhv_tripdata';
+DESCRIBE "fhvhv_tripdata";
+
+SELECT 'green_tripdata';
+DESCRIBE "green_tripdata";
+
+SELECT 'yellow_tripdata';
+DESCRIBE "yellow_tripdata";
--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,21 +1,14 @@
-/* creating 4 tables using tables from /data/data/taxi */
+/* creating 7 tables using tables from /data/data/taxi */
 
-create table yellow_tripdata as select * from 
-read_parquet('./data/data/taxi/yellow_tripdata*.parquet',  -- extracting all records from tables in data/taxi that have table names like yellow_tripdata...
-filename=True); --adds a column in the newly created table to indicate where each row of record is extracted from 
+-- load taxi data from parquet files, merging columns by name
+create table yellow_tripdata as select * from read_parquet('./data/taxi/yellow_tripdata.parquet', union_by_name=True, filename=True);
+create table green_tripdata as select * from read_parquet('./data/taxi/green_tripdata.parquet', union_by_name=True, filename=True);
+create table fhvhv_tripdata as select * from read_parquet('./data/taxi/fhvhv_tripdata.parquet', union_by_name=True, filename=True);
+create table fhv_tripdata as select * from read_parquet('./data/taxi/fhv_tripdata.parquet', union_by_name=True, filename=True);
 
-create table green_tripdata as select * from read_parquet('./data/data/taxi/green_tripdata*.parquet', filename=True); 
+-- load location & weather data from CSV files, merging columns by name and storing as strings
+create table central_park_weather as select * from read_csv_auto('./data/central_park_weather.csv', union_by_name=True, filename=True, all_varchar=1);
+create table fhv_bases as select * from  read_csv_auto('./data/fhv_bases.csv', union_by_name=True, filename=True, all_varchar=1, header=True);
 
-create table fhvhv_tripdata as select * from read_parquet('./data/data/taxi/fhvhv_tripdata*.parquet', filename=True); 
-
-create table fhv_tripdata as select * from read_parquet('./data/data/taxi/fhv_tripdata*.parquet', filename=True); 
-
-/* creating 2 tables using tables from /data/data */
-
-create table central_park_weather as select * from read_csv('data/data/central_park_weather.csv', filename = true);
-
-create table fhv_bases as select * from read_csv('data/data/fhv_bases.csv', filename = true);
-
-/* creating 1 tables using table from /data/data/citibike-tripdata.csv.gz */
-
-create table citibike_tripdata as select * from read_csv_auto('data/data/citibike-tripdata.csv.gz'); 
+-- load bike data from CSV files, merging columns by name and storing as strings
+create table bike_data as select * from read_csv_auto('./data/citibike-tripdata.csv.gz', union_by_name=True, filename=True, all_varchar=1);
